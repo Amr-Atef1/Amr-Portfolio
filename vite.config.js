d32505cb6a8cfa5da84ac8eee83bff7a5dbc2b9a
amratef <amr316671@gmail.com>
2025-10-23 08:48:54 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.JPG'],
-  base: './',
+  base: process.env.VITE_BASE_PATH || "/Amr-Portfolio",
 })
